77dd7ad622df8a755f0936140381c16e026063e7
Chalermchai Wauwai <59055693+phetchalermchai@users.noreply.github.com>
2025-04-22 07:21:16 +0700

    commit

--- app/(private)/admin/news/activities/skeleton/page.tsx
+++ app/(private)/admin/news/activities/skeleton/page.tsx
@@ -11,7 +11,7 @@ const TiptapEditor: React.FC = () => {
     content: "<p>เริ่มต้นเขียนข่าวของคุณที่นี่...</p>",
     editorProps: {
       attributes: {
-        class: 'p-2 border  border-solid border-base-200 rounded-lg cursor-text focus:ring-2 focus:ring-primary-500 outline-none focus:outline-offset-2',
+        class: 'p-2 min-h-24 border input-bordered rounded-lg focus:ring-primary focus:ring-offset-base-100 focus:ring focus:ring-offset-1 focus:outline-none', 
       },
     },
   });
@@ -20,8 +20,8 @@ const TiptapEditor: React.FC = () => {
     <div className="bg-base-100 rounded-lg shadow m-3 p-2 sm:m-3 sm:p-3 lg:m-4 lg:p-3 xl:m-5 xl:p-5">
       <div className="py-2 px-1 "><span className="text-sm">รายละเอียดกิจกรรม</span></div>
       {/* แสดง Editor Content */}
-      <input className="input input-bordered w-full bg-op"/>
-          <EditorContent editor={editor} />
+      <input className="input input-bordered w-full bg-op" />
+      <EditorContent editor={editor} />
     </div>
   );
 };
